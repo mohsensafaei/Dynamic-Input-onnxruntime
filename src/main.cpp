9cbc62601314d161ac858cf4d6384f6a1331b1d9
Mohsen Safaei <85816278+mohsensafaei@users.noreply.github.com>
2024-02-23 14:55:52 +0330

    Create main

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "runModel.h"
+
+
+int main()
+{
+    
+    std::string modelPath{"/mnt/c/Users/Mohsen/Desktop/Ubuntu/dynamic_input_onnxruntime/example.onnx"};
+    run_onnx_model(modelPath);
+
+
+    return 0;
+}
